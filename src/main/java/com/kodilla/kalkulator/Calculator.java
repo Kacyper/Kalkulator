42754f8198976b1b4a510385525b10789ee994f1
Kacper <kksykiewicz@gmail.com>
2021-08-27 19:09:49 +0200

    Pierwsza wersja

---
+++ src/main/java/com/kodilla/kalkulator/Calculator.java
@@ -0,0 +1,31 @@
+package com.kodilla.kalkulator;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+class Calculator {
+
+    Scanner scanner = new Scanner(System.in);
+    int firstNumber = scanner.nextInt();
+    int secondNumber = scanner.nextInt();
+
+    public void dodawanie() {
+        System.out.println("Suma liczb to: " + (firstNumber + secondNumber));
+    }
+
+    public void odejmowanie() {
+        System.out.println("Różnica liczb to: " + (firstNumber - secondNumber));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculator.class, args);
+        System.out.println("Podaj dwie liczby:");
+        Calculator calculator = new Calculator();
+        calculator.dodawanie();
+        calculator.odejmowanie();
+    }
+}
